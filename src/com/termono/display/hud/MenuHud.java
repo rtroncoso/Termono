ec8c75f7f613b15d7edacec6d71f11327ae06faf
Roberto <btuso.95@gmail.com>
2012-05-02 14:20:08 -0300

    Menu con batches que no funca bien

--- src/com/termono/display/hud/MenuHud.java
+++ src/com/termono/display/hud/MenuHud.java
@@ -1,122 +1,279 @@
 package com.termono.display.hud;
 
+
+
 import org.andengine.engine.camera.hud.HUD;
+
 import org.andengine.engine.handler.IUpdateHandler;
+
 import org.andengine.entity.sprite.Sprite;
+
+import org.andengine.entity.sprite.batch.DynamicSpriteBatch;
+
+import org.andengine.entity.sprite.batch.SpriteBatch;
+
+import org.andengine.entity.text.Text;
+
 import org.andengine.input.touch.TouchEvent;
+
 import org.andengine.opengl.texture.TextureOptions;
+
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+
 import org.andengine.opengl.texture.region.ITextureRegion;
+
+
 
 import com.termono.game.Game;
 
+
+
 public class MenuHud extends HUD{
 
+
+
 		// ===========================================================
+
 		// Constants
+
 		// ===========================================================
+
 	
+
 		// ===========================================================
+
 		// Fields
+
 		// ===========================================================
+
 		private Game mGame;
+
 		private BitmapTextureAtlas mMenuTextureAtlas;
-		private ITextureRegion mMenuTextureRegion;
-		private Sprite mMenuSprite;
+
 		private HUD mHud;
+
 		private IUpdateHandler pUpdatehandler;
+
 		private boolean updater = false;
+
+		private ITextureRegion mMenuTextureRegion;
+
+		private ITextureRegion mCancelTextureRegion;
+
+		private Sprite mMenuSprite;
+
+		private Sprite mCancelSprite;
+
+		private SpriteBatch mMenuBatch;
+
 		
+
 		// ===========================================================
+
 		// Constructor
+
 		// ===========================================================
+
 		public MenuHud(Game pGame, HUD pHud){
+
 			//Init local Variables
+
 			this.mGame = pGame;
+
 			this.mHud = pHud;
+
 			
+
 			// Set base path for Textures
+
 			BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/Interfaces/");
+
 			
+
 			// Create Texture objects
+
 			this.mMenuTextureAtlas = new BitmapTextureAtlas(this.mGame.getTextureManager(), 512, 512, TextureOptions.BILINEAR);		
+
 			this.mMenuTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mMenuTextureAtlas, this.mGame.getApplicationContext(), "menu.png", 0, 0);
+
+			this.mCancelTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mMenuTextureAtlas, this.mGame.getApplicationContext(), "cruz.png", 0, 320);
+
 			
+
 			// Load Texture into memory and on the screen
+
 			this.mMenuTextureAtlas.load();
+
+
+
 			
-			float posX = this.mGame.getDisplay().getCamera().getWidth()-96;
-			float posY = -236;
-			this.mMenuSprite = new Sprite(posX, posY, this.mMenuTextureRegion, this.mGame.getVertexBufferObjectManager()) {
-			
-		
-					
+
+			this.mMenuSprite = new Sprite(0, 0, this.mMenuTextureRegion, this.mGame.getVertexBufferObjectManager()) {
+
 				@Override
-				public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+
+				public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
+
+						float pTouchAreaLocalX, float pTouchAreaLocalY) {
+
+					// TODO Auto-generated method stub
+
 					switch(pSceneTouchEvent.getAction()) {
+
 						case TouchEvent.ACTION_DOWN:
-							if(updater == false){		
-							MenuHud.this.mGame.getScene().registerUpdateHandler(pUpdatehandler = new IUpdateHandler() 
-							{
-								
-								
-								@Override
-								public void onUpdate(float pSecondsElapsed) {
-									updater = true;
-									if(MenuHud.this.mMenuSprite.getX() < 230){MenuHud.this.mGame.getScene().unregisterUpdateHandler(pUpdatehandler);}//updater=false;}// MenuHud.this.mMenuSprite.setPosition(MenuHud.this.mMenuSprite.getInitialX(), MenuHud.this.mMenuSprite.getInitialY());}
-									MenuHud.this.mMenuSprite.setPosition(MenuHud.this.mMenuSprite.getX() - 20, MenuHud.this.mMenuSprite.getY() + 12);
-								}
-								
-								@Override
-								public void reset() {
-									// TODO Auto-generated method stub			
-								}
-								
-							});		
-						}else if(updater==true){updater=false; MenuHud.this.mMenuSprite.setPosition(MenuHud.this.mMenuSprite.getInitialX(), MenuHud.this.mMenuSprite.getInitialY());}
-							
-									break;
+
+							if(updater == false) {		
+
+								MenuHud.this.mGame.getScene().registerUpdateHandler(pUpdatehandler = new IUpdateHandler() {
+
 									
+
+									@Override
+
+									public void onUpdate(float pSecondsElapsed) {
+
+										updater = true;
+
+										if(MenuHud.this.mMenuBatch.getX() < 230){MenuHud.this.mGame.getScene().unregisterUpdateHandler(pUpdatehandler);}//updater=false;}// MenuHud.this.mMenuSprite.setPosition(MenuHud.this.mMenuSprite.getInitialX(), MenuHud.this.mMenuSprite.getInitialY());}
+
+										MenuHud.this.mMenuBatch.setPosition(MenuHud.this.mMenuBatch.getX() - 20, MenuHud.this.mMenuBatch.getY() + 12);
+
+									}
+
 									
+
+									@Override
+
+									public void reset() {
+
+										// TODO Auto-generated method stub			
+
+									}
+
+								});		
+
+							} else if(updater == true) { updater = false; MenuHud.this.mMenuBatch.setPosition(MenuHud.this.mMenuBatch.getInitialX(), MenuHud.this.mMenuBatch.getInitialY()); }
+
+						break;
+
+									
+
+									
+
 						case TouchEvent.ACTION_MOVE:
-						  /*this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2, pSceneTouchEvent.getY() - this.getHeight() / 2);
-							MenuHud.this.mMenuSprite.setAlpha((pSceneTouchEvent.getX() + pSceneTouchEvent.getY()) / 1200);
-							MenuHud.this.mMenuSprite.setScale((pSceneTouchEvent.getX() + pSceneTouchEvent.getY()) / 800);
-							MenuHud.this.mGame.getStatsHud().getTermono().setText("X: " + String.valueOf(pSceneTouchEvent.getX()) + "\n Y: " + String.valueOf(pSceneTouchEvent.getY()) + "\n Alpha: " + String.valueOf(MenuHud.this.mMenuSprite.getAlpha()));
-							break;*/
+
 						case TouchEvent.ACTION_CANCEL:
+
 						case TouchEvent.ACTION_OUTSIDE:
+
 						case TouchEvent.ACTION_UP:
-						/*	MenuHud.this.mMenuSprite.setPosition(MenuHud.this.mMenuSprite.getInitialX(), MenuHud.this.mMenuSprite.getInitialY());
-							MenuHud.this.mMenuSprite.setScale(1.0f);
-							MenuHud.this.mMenuSprite.setAlpha(0.3f);
-							MenuHud.this.mGame.getStatsHud().getTermono().setText("X: " + String.valueOf(MenuHud.this.mMenuSprite.getInitialX()) + "\n Y: " + String.valueOf(MenuHud.this.mMenuSprite.getInitialY()));*/
+
 							break;
+
 					}
-							return true;
+
+					return true;
+
 				}
+
 			};
-			this.mHud.registerTouchArea(this.mMenuSprite);
-			//this.mMenuSprite.setAlpha(0.3f);
-			//this.mMenuSprite.setScale(1.0f); 
+
 			
-			
-			
+
+			this.mCancelSprite = new Sprite(416, 27, this.mCancelTextureRegion, this.mGame.getVertexBufferObjectManager());/* {
+
+
+
+				@Override
+
+				public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+
+					switch(pSceneTouchEvent.getAction()) {
+
+						case TouchEvent.ACTION_DOWN:
+
+							MenuHud.this.mCancelSprite.setScale(2.0f);
+
+							break;
+
+						case TouchEvent.ACTION_MOVE:
+
+						case TouchEvent.ACTION_CANCEL:
+
+						case TouchEvent.ACTION_OUTSIDE:
+
+						case TouchEvent.ACTION_UP:
+
+							MenuHud.this.mCancelSprite.setScale(1.0f);
+
+							break;
+
+					}
+
+					return true;
+
+				}
+
+			};
+
+			this.mHud.registerTouchArea(this.mCancelSprite);*/
+
+
+
+			float posX = this.mGame.getDisplay().getCamera().getWidth() - 96;
+
+			this.mMenuBatch = new DynamicSpriteBatch(posX, -236, this.mMenuTextureAtlas, 2, this.mGame.getVertexBufferObjectManager()) {
+
+				@Override
+
+				public boolean onUpdateSpriteBatch() {
+
+					this.draw(MenuHud.this.mMenuSprite);
+
+					this.draw(MenuHud.this.mCancelSprite);
+
+					MenuHud.this.mHud.registerTouchArea(MenuHud.this.mMenuSprite);
+
+					return true;
+
+				}
+
+				
+
+			};
+
 		}
 
 
+
+
+
 		
+
 		// ===========================================================
+
 		// Methods
+
 		// ===========================================================
+
 		
+
 		// ===========================================================
+
 		// Getter & Setter
+
 		// ===========================================================
-		public Sprite getMenuSprite() {
-			return this.mMenuSprite;
+
+		public SpriteBatch getMenuBatch() {
+
+			return this.mMenuBatch;
+
 		}
+
 	
+
 		
+
 }
