d9c99b5d7aa898883fa991955e7d5e67673b7a0c
raccooney <btuso.95@gmail.com>
2012-05-09 11:58:12 -0300

    inventario

--- src/com/termono/scenes/InventoryScene.java
+++ src/com/termono/scenes/InventoryScene.java
@@ -1,6 +1,15 @@
 package com.termono.scenes;
 
+import org.andengine.engine.camera.hud.HUD;
+import org.andengine.entity.Entity;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
+
+import com.termono.display.hud.MenuHud;
+import com.termono.game.Game;
 
 public class InventoryScene extends Scene {
 	// ===========================================================
@@ -11,12 +20,26 @@ public class InventoryScene extends Scene {
 	// ===========================================================
 	// Fields
 	// ===========================================================
-
+	private Game mGame;
+	private HUD mHud;
+	private BitmapTextureAtlas mInvetoryTextureAtlas;
+	private ITextureRegion mInventoryTextureRegion;
+	private Sprite mInventorySprite;
+	private Entity mInventoryEntity;
 	
 	// ===========================================================
 	// Constructors
 	// ===========================================================
-
+	public InventoryScene(Game pGame){
+		this.mGame = pGame;
+		this.mHud = new HUD();	
+		
+		
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/Interfaces/");
+		
+		
+	}
+	
 	
 	// ===========================================================
 	// Methods for/from SuperClass/Interfaces
